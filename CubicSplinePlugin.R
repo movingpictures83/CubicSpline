26205ceb2e4412d9e99961689ca1358439777cbd
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-01-20 08:40:17 -0500

    Made more flexible

--- CubicSplinePlugin.R
+++ CubicSplinePlugin.R
@@ -8,54 +8,30 @@ input <- function(inputfile) {
   rownames(parameters) <<- parameters[,1]
   t1 <<- read.csv(toString(parameters["training",2]), sep = "\t", header = FALSE, nrow = 100)
   t2 <<- read.csv(toString(parameters["clinical",2]), sep="\t", header = TRUE) 
-  #pc <<- read.csv(inputfile, header = TRUE);
+  joincol <<- toString(parameters["joinby", 2])
+  id <<- toString(parameters["id", 2])
+  xcoor <<- toString(parameters["x", 2])
 }
 
 
 run <- function() {
   t1 <<- as.data.frame(t(t1))
-  colnames(t1)[1] <<- "CEL"
-  x <<- merge(t1, t2, by = "CEL")
-  subID <<- unique(x$SUBJECTID)
+  colnames(t1)[1] <<- joincol
+  x <<- merge(t1, t2, by = joincol)
+  subID <<- unique(as.character(unlist(x[id])))
 }
 
-output <- function(outputfile) {
+output <- function(outputfile) { 
    for(y in subID){
-    pat = x[x[,"SUBJECTID"]==y,]
-    pat = pat[order(pat$TIMEHOURS),]
+    pat = x[x[,id]==y,]
+    pat = pat[order(as.numeric(unlist(pat[xcoor]))),]
     v1 = as.vector(pat[,2])
-    plot(as.vector(pat$TIMEHOURS), v1)
-    v.spl = smooth.spline(pat$TIMEHOURS, v1)
+    plot(as.vector(as.numeric(unlist(pat[xcoor]))), v1)
+    v.spl = smooth.spline(as.numeric(unlist(pat[xcoor])), v1)
     plot(v.spl$x, v.spl$y)
     write.table(v.spl$x, file = outputfile, sep = ",", append=TRUE, row.names = TRUE, col.names = TRUE)
     write.table(v.spl$y, file = outputfile, sep = ",", append=TRUE, row.names = TRUE, col.names = TRUE)
   }
 }
-
-
-#setwd("appData/Res_Challenge/Data")
-#t1 = read.table("ViralChallenge_training_EXPRESSION_RMA.tsv", sep = "\t", header = FALSE, nrow = 100)
-#t2 <- read.table("ViralChallenge_training_CLINICAL.tsv", sep="\t", header = TRUE)
-
-
-# t1 = apply(t1, 1, as.vector)
-# t2 = apply(t2, 1, as.vector)
-#t1 = as.data.frame(t(t1))
-#colnames(t1)[1] <- "CEL"
-#x = merge(t1, t2, by = "CEL")
-#subID = unique(x$SUBJECTID)
-
-#for(y in subID){
-#  pat = x[x[,"SUBJECTID"]==y,]
-#  pat = pat[order(pat$TIMEHOURS),]
-  # pat[23, 15] = 172
-#  v1 = as.vector(pat[,2])
-#  plot(as.vector(pat$TIMEHOURS), v1)
-#  v.spl = smooth.spline(pat$TIMEHOURS, v1)
-  # v.spl = apply(as.vector(pat$TIMEHOURS), 2, smooth.spline, x = as.vector(v1[,1]))
-#  plot(v.spl$x, v.spl$y)
-#  write.table(v.spl$x, file = "dataCheck.csv", sep = ",", append=TRUE, row.names = TRUE, col.names = TRUE)
-#  write.table(v.spl$y, file = "dataCheck.csv", sep = ",", append=TRUE, row.names = TRUE, col.names = TRUE)
-#}
 
 
